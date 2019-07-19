d0f8a4da0df529c30ebbaa65f17ef67172ed8673
cuiwei34 <cuiwei34@jd.com>
2019-07-19 16:26:27 +0800

    jni test

---
+++ com.cuiwjava/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2019/6/5 16:01
+ * @Created by cuiwei34
+ */
+public class Test {
+	public static void main(String[] args) {
+		"1444".split(",");
+	}
+}
